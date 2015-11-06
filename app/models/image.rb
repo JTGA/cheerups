e6a6ab85b1bcac8d1295b233b1df9db1baa07914
J T <njthood@yahoo.com>
2015-11-05 21:03:40 -0800

    html update

--- app/models/image.rb
+++ app/models/image.rb
@@ -1,5 +1,6 @@
 class Image < ActiveRecord::Base
   has_many :comments, dependent: :destroy
+  has_many :likes
   validates :uri, presence: true,
                     length: { minimum: 5 }
   has_attached_file :upload, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
